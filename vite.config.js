6b4982179f1f670819c3ad6975ef4aa5a16e2d9b
AVGV1985 <antonio.vasco.valentim@gmail.com>
2025-08-08 13:20:19 +0100

    Set Vite base for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/joao-art-site/',   // ← MUITO IMPORTANTE para GitHub Pages
   server: {
     host: true,         // ← Isto ativa o acesso externo!
     port: 5173, // opcional, mas deixa explícito
