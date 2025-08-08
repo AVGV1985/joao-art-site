539e49b15f9d2f7d1b7b1143fba69c7ec363165f
AVGV1985 <antonio.vasco.valentim@gmail.com>
2025-08-08 14:51:18 +0100

    BrowserRouter + basename + 404 fallback for GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import './index.css';
-import './i18n'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename="/joao-art-site">
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
-);
+)
